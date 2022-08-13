ff283ec197a0b6933f4104b6dc2bd5f2b6e330d4
cherethod <sydig.reus@gmail.com>
2022-08-13 16:24:46 +0200

    improvements

--- games/3inArow/js/main.js
+++ games/3inArow/js/main.js
@@ -2,6 +2,8 @@ const input = document.getElementById('playername_input');
 const keyboardKey = document.querySelectorAll('.keyboard__key');
 let playerOneName = 'Jugador 1';
 let playerTwoName = 'Jugador 2';
+let avatarsPlayerOne = [];
+let avatarsPlayerTwo = [];
 let playerOneSymbol = undefined;
 let playerTwoSymbol = undefined;
 let currentPlayer = undefined;
@@ -28,7 +30,18 @@ const player = {
 class game {
     constructor (name, status) {        
     this.gameName = name;
-    this.gameStatus = status;
+    this.gameStatus = status;    
+    }
+    resetValues() {        
+        playerOneName = 'Jugador 1';
+        playerTwoName = 'Jugador 2';
+        avatarsPlayerOne = [];
+        avatarsPlayerTwo = [];
+        playerOneSymbol = undefined;
+        playerTwoSymbol = undefined;
+        currentPlayer = undefined;
+        round = 0;
+        winner = false;
     }
 
 }
@@ -36,6 +49,14 @@ class game {
 class tresEnRaya extends game {
     constructor (name, status) {
     super (name, status);
+    }
+    cleanGrid() {
+        let cells = document.querySelectorAll('.game__grid');
+        cells.forEach(cell => {
+            cell.innerHTML='';
+            cell.classList.toggle('grid--disabled');
+            cell.style.backgroundColor='#FFF';
+        });
     }
     launchGame() {
         document.querySelector('.lobby').style.display='none';
@@ -75,13 +96,29 @@ class tresEnRaya extends game {
     }
     setPlayerAvatar(elem) {
         let selectedAvatar = elem.target.attributes.src.value;
+        function colectPlayerAvatars () {
+            let mainAvatar = selectedAvatar
+            let cutSrc = mainAvatar.indexOf('__');
+            mainAvatar = mainAvatar.split('',cutSrc);
+            let baseSrc = mainAvatar.join('');
+            let loseAvatar = baseSrc+'__lose.png';
+            let winAvatar = baseSrc+'__win.png';
+            if (currentPlayer===1) {
+                avatarsPlayerOne.push(selectedAvatar, loseAvatar, winAvatar)
+            } else {
+                avatarsPlayerTwo.push(selectedAvatar, loseAvatar, winAvatar);
+            }
+        }
         if (currentPlayer===1) {
-            document.getElementById('player1_avatar').src=selectedAvatar;
+            document.getElementById('player1_avatar').src=selectedAvatar;        
+            colectPlayerAvatars();
+            // alert()
             currentPlayer=2;
             document.querySelector('.current__player').innerHTML=playerTwoName;            
         } else {            
             document.getElementById('player2_avatar').src=selectedAvatar;
             document.querySelector('.select__avatar').style.display='none';
+            colectPlayerAvatars();
             currentPlayer=1
             this.gameStatus = 'select symbol';
             this.setSymbol();
@@ -120,20 +157,18 @@ class tresEnRaya extends game {
         })
     }
     checkCell(cell){
-        (round<10 && winner==false && cell.target.innerHTML === '') ?  (
+        (round<10 && winner===false && cell.target.innerHTML === '') ?  (
             (currentPlayer===1) ? (
                 cell.target.innerHTML=playerOneSymbol,
                 round++,
                 document.querySelector('.game__container--player1').style.opacity='0.2',
-                document.querySelector('.game__container--player2').style.opacity='1',
-                currentPlayer = 2     ,           
+                document.querySelector('.game__container--player2').style.opacity='1',          
                 document.getElementById('player_in_turn').innerHTML=  playerTwoName
             ) : (
                 cell.target.innerHTML=playerTwoSymbol,
                 round++,
                 document.querySelector('.game__container--player1').style.opacity='1',
                 document.querySelector('.game__container--player2').style.opacity='0.2',
-                currentPlayer = 1,
                 document.getElementById('player_in_turn').innerHTML= playerOneName
             )
         ) : alert('Esta casilla ya está en uso!');
@@ -160,45 +195,96 @@ class tresEnRaya extends game {
                 // console.log(cell1, cell2, cell3)
 // 
                 if (cell1 === cell2 && cell1 ===cell3) {
-                    alert()
-                    this.winner = playerOneName;
+                    // alert()
+                    winner = 1;
                     this.gameStatus = 'game end';
-                    document.getElementById(`game_grid_${val1}`).style.backgroundColor='rgba(28, 182, 28, .60)'
-                    document.getElementById(`game_grid_${val2}`).style.backgroundColor='rgba(28, 182, 28, .60)'
-                    document.getElementById(`game_grid_${val3}`).style.backgroundColor='rgba(28, 182, 28, .60)'
+                    document.getElementById(`game_grid_${val1}`).style.backgroundColor='rgba(28, 182, 28, .60)';
+                    document.getElementById(`game_grid_${val2}`).style.backgroundColor='rgba(28, 182, 28, .60)';
+                    document.getElementById(`game_grid_${val3}`).style.backgroundColor='rgba(28, 182, 28, .60)';
+                    this.gameFinished();
+                } else {
+                    currentPlayer = 2;
                 }
             
             }
         } else {
             for (let i = 0; i < targetRows.length; i++) {
                 const cells = targetRows[i];
-                console.log(cells)
                 let val1 = cells[0];
                 let val2 = cells[1];
                 let val3 = cells[2];
                 let cell1 = document.getElementById(`game_grid_${val1}`).innerHTML;
                 let cell2 = document.getElementById(`game_grid_${val2}`).innerHTML;
                 let cell3 = document.getElementById(`game_grid_${val3}`).innerHTML;
-                // console.log(cell1, cell2, cell3)
 
                 if (cell1 === cell2 && cell1 ===cell3) {
-                    alert()
-                    this.winner = playerTwoName;
+                    // alert()
+                    winner = 2;
                     this.gameStatus = 'game end';
-                    document.getElementById(`game_grid_${val1}`).style.backgroundColor='rgba(28, 182, 28, .60)'
-                    document.getElementById(`game_grid_${val2}`).style.backgroundColor='rgba(28, 182, 28, .60)'
-                    document.getElementById(`game_grid_${val3}`).style.backgroundColor='rgba(28, 182, 28, .60)'
-                }
-            
+                    document.getElementById(`game_grid_${val1}`).style.backgroundColor='rgba(28, 182, 28, .60)';
+                    document.getElementById(`game_grid_${val2}`).style.backgroundColor='rgba(28, 182, 28, .60)';
+                    document.getElementById(`game_grid_${val3}`).style.backgroundColor='rgba(28, 182, 28, .60)';
+                    this.gameFinished();
+                } else {                    
+                    currentPlayer = 1;
+                }            
             }
         }
+    }
+    gameFinished() {
+        let grids = document.querySelectorAll('.game__grid');
+        let buttons = document.querySelectorAll('.cta--finish');
+        grids.forEach(grid => grid.classList.toggle('grid--disabled'));        
+        document.querySelector('.game__container--player1').style.opacity='1';
+        document.querySelector('.game__container--player2').style.opacity='1';
+        document.querySelector('.game__paragraph').innerHTML='HA GANADO EL JUGADOR: ';
+        (winner==1) ? document.querySelector('.game__paragraph').innerHTML+=`<span id='player_in_turn'>${playerOneName}<span/>` : document.querySelector('.game__paragraph').innerHTML+=`<span id='player_in_turn'>${playerTwoName}<span/>`
+        //  AVATARS [ default , lose , win ]  //
+        switch (winner) {
+            case 0:
+                document.querySelector('.game__paragraph').innerHTML='EMPATE';
+                document.getElementById('player1_avatar_ig').src=avatarsPlayerOne[1];
+                document.getElementById('player2_avatar_ig').src=avatarsPlayerTwo[1];
+                break;
+        
+            case 1:
+                document.getElementById('player1_avatar_ig').src=avatarsPlayerOne[2];
+                document.getElementById('player2_avatar_ig').src=avatarsPlayerTwo[1];
+                break;
+            
+            case 2:
+                document.getElementById('player1_avatar_ig').src=avatarsPlayerOne[1];
+                document.getElementById('player2_avatar_ig').src=avatarsPlayerTwo[2];
+                break;
+        }
+        buttons.forEach(btn => btn.style.display='inline-block')
     }
     setRound(cell) {
         this.checkCell(cell)
         this.checkRound(cell)
-        if (round === 9) {winner = 0;
-            document.querySelector('.game__container').style.display='none';
+        if (round === 9 && winner==false) {
+            winner = 0;
+            this.gameFinished();
         }
+    }
+    exitGame() {        
+        this.cleanGrid();
+        this.resetValues();
+        document.querySelector('.game__container').style.display='none';        
+        document.querySelector('.lobby').style.display='flex';
+
+    }
+    nextGame() {        
+        this.cleanGrid();
+        currentPlayer = 1;
+        round = 0;
+        winner = false;
+        document.getElementById('player1_avatar_ig').src=avatarsPlayerOne[0];
+        document.getElementById('player2_avatar_ig').src=avatarsPlayerTwo[0];
+        document.querySelector('.game__container').style.display='grid';        
+        document.querySelector('.lobby').style.display='none';
+        document.querySelector('.cta__flex--right').display='none';
+
     }
 }
 
