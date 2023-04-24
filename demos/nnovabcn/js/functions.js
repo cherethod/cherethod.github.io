c6555140ce27b22f8bdc1c5c03abfd45468fffc3
cherethod <sydig.reus@gmail.com>
2023-04-24 18:52:04 +0200

    fix styles added faqs

--- demos/nnovabcn/js/functions.js
+++ demos/nnovabcn/js/functions.js
@@ -48,106 +48,33 @@ const toggleColorMode = () => {
   
     colorModeBtn.addEventListener('click', toggleColorMode);
     setColorMode(storedMode);
+  }; 
+  
+  const openFaqAnswer = () => {
+    function closeAnswers(currentTarget) {
+      const answers = document.querySelectorAll('.faqs__answer--container');
+      answers.forEach((faq) => {
+        if (faq.classList.contains('open') && faq !== currentTarget) {
+          faq.classList.remove('open');
+        }
+      });
+    }
+    const answersBtn = document.querySelectorAll('.question__open--icon');
+    answersBtn.forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        let parent = e.target.closest('.faqs__answer--container');
+        closeAnswers(parent);
+        parent.classList.contains('open') ? parent.classList.remove('open') : parent.classList.add('open');
+        console.log(parent.parentElement);
+      });
+    });
   };
+  
   
   document.addEventListener('DOMContentLoaded', function () {
     initColorMode();
-    createSliderObject();
+    openFaqAnswer();
+    // createSliderObject();
   });
 
-
-
-
-// let colorModeToggleListener = function() {
-//     let colorModeBtn = document.querySelector('.toggle__mode--container');
-//     colorModeBtn.addEventListener('click', (e)=> {
-//         // console.log(e.target)
-//         if (e.target.classList.contains('toggle__mode--btn')) {
-//             if (e.target.classList.contains('dark')) {
-//                 e.target.classList.remove('dark');
-//                 e.target.classList.add('light');
-//                 storeColorMode('light');
-//                 setColorMode()
-//             } else {
-//                 e.target.classList.remove('light');
-//                 e.target.classList.add('dark');
-//                 storeColorMode('dark');
-//                 setColorMode()
-//             }
-//         }
-//     })
-// }
-
-// const checkColorMode = ()=> {
-//     if (!localStorage.getItem('color-mode')) storeColorMode('dark'); 
-//     return localStorage.getItem('color-mode');
-// }
-
-// const storeColorMode = (mode = undefined)=> {
-//     if (mode != undefined && mode != null && localStorage.getItem('color-mode')) {
-//         localStorage.setItem('color-mode', mode);
-//     }
-//     else if (mode == undefined || mode == null || mode != 'dark' || mode != 'light' || !localStorage.getItem('color-mode')) {
-//         localStorage.setItem('color-mode', 'dark');
-//     }
-// }
-
-// const setColorMode = ()=> {
-//     let container = document.querySelector('.container');
-//     let colorModeBtn = document.querySelector('.toggle__mode--btn');
-//     let logo = document.querySelector('.logo__img');
-//     if (checkColorMode() == 'dark') {
-//         if (container.classList.contains('light')) container.classList.remove('light');
-//         container.classList.add('dark');
-//         if (colorModeBtn.classList.contains('light')) colorModeBtn.classList.remove('light');
-//         colorModeBtn.classList.add('dark');
-//         logo.src='./img/Logo_Nnova_white_very-small.png';
-//     }
-//     else if (checkColorMode() == 'light') {
-//         if (container.classList.contains('dark')) container.classList.remove('dark');
-//         container.classList.add('light');  
-//         if (colorModeBtn.classList.contains('dark')) colorModeBtn.classList.remove('dark');
-//         colorModeBtn.classList.add('light'); 
-//         logo.src='./img/Logo_Nnova_black_very-small.png';
-//     }
-//     // console.log(colorModeBtn.children)
-// }
-
-// const initColorMode = function() {
-//     try {
-//         colorModeToggleListener()
-//         checkColorMode()
-//         setColorMode();
-//     } catch (error) {
-//         console.error('Ha ocurrido un error al inicializar el alternador de color', error)
-//     }
-    
-// }
-
-// document.addEventListener('DOMContentLoaded', initColorMode);
-// (function(){initColorMode()})
-
-// const setColorMode = (mode = undefined)=> {
-//     let container = document.querySelector('.container');
-//     if (mode == undefined && !localStorage.getItem('color-mode')){
-//         localStorage.setItem('color-mode', 'dark');
-//         if (container.classList.contains('light')) container.classList.remove('light');
-//         container.classList.add('dark');
-//     } 
-//     else if (localStorage.getItem('color-mode') && !container.classList.contains(mode)) {
-//         if (container.classList.contains('light') && mode == 'dark') container.classList.remove('light');
-//         if (container.classList.contains('dark') && mode == 'light') container.classList.remove('dark');
-//         // (mode == 'dark') ? container.classList.remove('light') : container.classList.remove('dark');
-//         container.classList.add(localStorage.getItem('color-mode'))
-//     }
-//     else if (!localStorage.getItem('color-mode') && (mode == 'dark' || mode == 'light')) {
-//         localStorage.setItem('color-mode', mode);
-//         (mode == 'dark') ? (
-//             container.classList.add('dark'),
-//             container.classList.remove('light')
-//             ) : (
-//                 container.classList.add('light'),
-//                 container.classList.remove7('dark')
-//             )
-//     }
-// }
+ 
